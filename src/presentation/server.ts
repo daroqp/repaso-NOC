e3189bc1c9f1c92138df8f6d68dbccc99a8d7155
Dario Quispe <daro.qp@gmail.com>
2026-01-30 21:31:49 -0300

    feat: add cron coupled

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,20 @@
+import CronJob = require("cron");
+
 class Server {
 
     public static start(){
 
         console.log("Server started...");
 
+        const job = new CronJob.CronJob(
+            '*/2 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('2 second', date);
+            },
+        );
+
+        job.start()
     }
 }
 
