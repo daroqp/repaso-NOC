cebf7c00cfdf2943bef3636533a66abb86e76495
Dario Quispe <daro.qp@gmail.com>
2026-02-05 16:28:18 -0300

    add: new function in log entity

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -11,5 +11,17 @@ export class LogEntity {
         this.level = level;
         this.createdAt = new Date();
     }
+
+    static fromJson = ( json: string ): LogEntity => {
+    const { message, level, createdAt } = JSON.parse( json );
+
+    // es recomendable hacerlo para todos los parametros
+    if( !message ) throw new Error('Message is required')
+
+    const log = new LogEntity( message, level);
+    log.createdAt = new Date( createdAt )
+
+    return log
+  }
 }
 
